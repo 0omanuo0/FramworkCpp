66c90900213afee426fe94e2c2e2cf46e6c0d5a6
0omanuo0 <manu365manu@gmail.com>
2024-06-14 21:46:33 +0000

    add support for JWT, UUIDv4, base64...

--- src/url_encoding.h
+++ src/url_encoding.h
@@ -6,10 +6,25 @@
 #pragma once
 #include <string>
 #include <regex>
+#include "base64.h"
 
 class UrlEncoding
 {
+private:
+    static std::string remove_base64_padding(const std::string &base64_str)
+    {
+        size_t padding_pos = base64_str.find('.');
+        if (padding_pos != std::string::npos)
+        {
+            return base64_str.substr(0, padding_pos);
+        }
+        return base64_str;
+    }
+
 public:
+    static std::string encodeURIbase64(const std::string &in) { return remove_base64_padding(base64_encode(in, true)); }
+    static std::string decodeURIbase64(const std::string &in) { return base64_decode(in); }
+
     static std::string decodeURIComponent(const std::string &url)
     {
         std::ostringstream decoded;

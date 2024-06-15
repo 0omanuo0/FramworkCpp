7537dd0a4246bc4070afabd72f73eda2a1130d4e
0omanuo0 <manu365manu@gmail.com>
2024-06-15 16:03:04 +0000

    feat: change the response class to be more logic and returnable by the endpoints

--- src/response.cpp
+++ src/response.cpp
@@ -1,69 +1,70 @@
-// #include "server.h"
+#include "response.h"
 
-// int HttpServer::sendResponse(int socket, std::string response)
-// {
+#ifndef HTTP_CODES
+    #define HTTP_CODES
+    namespace httpStatus
+    {
+        std::map<int, std::string> Code = {
+            {200, "HTTP/1.1 200 OK"},
+            {201, "HTTP/1.1 201 Created"},
+            {202, "HTTP/1.1 202 Accepted"},
+            {204, "HTTP/1.1 204 No Content"},
+            {300, "HTTP/1.1 300 Multiple Choices"},
+            {301, "HTTP/1.1 301 Moved Permanently"},
+            {302, "HTTP/1.1 302 Found"},
+            {303, "HTTP/1.1 303 See Other"},
+            {304, "HTTP/1.1 304 Not Modified"},
+            {307, "HTTP/1.1 307 Temporary Redirect"},
+            {308, "HTTP/1.1 308 Permanent Redirect"},
+            {400, "HTTP/1.1 400 Bad Request"},
+            {401, "HTTP/1.1 401 Unauthorized"},
+            {403, "HTTP/1.1 403 Forbidden"},
+            {404, "HTTP/1.1 404 Not Found"},
+            {405, "HTTP/1.1 405 Method Not Allowed"},
+            {500, "HTTP/1.1 500 Internal Server Error"},
+            {501, "HTTP/1.1 501 Not Implemented"},
+            {503, "HTTP/1.1 503 Service Unavailable"}
+        };
+    }
+#endif
 
-//     // Enviar la respuesta HTTP al cliente
-//     const char *httpResponse = response.c_str();
-//     if (send(socket, httpResponse, strlen(httpResponse), 0) < 0)
-//     {
-//         std::cerr << "Error al enviar la respuesta HTTP" << std::endl;
-//         return -1;
-//     }
+Response::Response(const std::string &responseMessage, int responseCode, std::map<std::string, std::string> headers)
+    :message(responseMessage), type(httpStatus::Code[responseCode]), headers(headers) {}
 
-//     return 0;
-// }
-// int HttpServer::sendResponse(SSL *ssl, std::string response)
-// {
+void Response::setIsFile(std::string contentType, int fileLength){
+    this->isFile = true;
+    this->headers["Content-Type"] = contentType;
+    this->headers["Content-Length"] = std::to_string(fileLength);
+}
 
-//     // Enviar la respuesta HTTP al cliente
-//     const char *httpResponse = response.c_str();
-//     if (SSL_write(ssl, httpResponse, strlen(httpResponse)) < 0)
-//     {
-//         std::cerr << "Error al enviar la respuesta HTTPS" << std::endl;
-//         return -1;
-//     }
-//     return 0;
-// }
 
-// int HttpServer::sendResponse(int socket, std::vector<std::string> response)
-// {
-//     if (response.empty())
-//         return -1;
+std::string Response::generateResponse()
+{
+    std::string response = type + "\r\n";
+    
 
-//     // Enviar el encabezado y el contenido del archivo al cliente
-//     if (send(socket, response[0].c_str(), response[0].size(), 0) < 0)
-//     {
-//         std::cerr << "Error al enviar el encabezado HTTP" << std::endl;
-//         return -1;
-//     }
 
-//     if (send(socket, response[1].c_str(), response[1].size(), 0) < 0)
-//     {
-//         std::cerr << "Error al enviar el contenido del archivo" << std::endl;
-//         return -1;
-//     }
+    if(!isFile){
+        auto length = message.length();
+        if (length > 0) headers["Content-Length"] = std::to_string(length);
 
-//     return 0;
-// }
+        if (headers["Content-Type"].empty()) headers["Content-Type"] = "text/html";
 
-// int HttpServer::sendResponse(SSL *ssl, std::vector<std::string> response)
-// {
-//     if (response.empty())
-//         return -1;
+        if (headers["Content-Length"].empty()) headers["Content-Length"] = "0";
+    }
 
-//     // Enviar el encabezado y el contenido del archivo al cliente
-//     if (SSL_write(ssl, response[0].c_str(), response[0].size()) < 0)
-//     {
-//         std::cerr << "Error al enviar el encabezado HTTPS" << std::endl;
-//         return -1;
-//     }
+    headers["Server"] = SERVER_VERSION;
 
-//     if (SSL_write(ssl, response[1].c_str(), response[1].size()) < 0)
-//     {
-//         std::cerr << "Error al enviar el contenido del archivo https" << std::endl;
-//         return -1;
-//     }
+    for (const auto &header : headers)
+        response += header.first + ": " + header.second + "\r\n";
 
-//     return 0;
-// }
+
+    std::string cookiesData = "Set-Cookie: ";
+    for (const auto &cookie : cookies)
+        cookiesData += cookie.first + "=" + cookie.second + "; ";
+
+    cookiesData += "\r\n";
+    response += cookiesData;
+
+    return response + "\r\n" + message;
+}
---
+++ src/response.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <string>
+#include <map>
+
+#ifndef SERVER_VALUES
+    #define SERVER_VALUES
+
+    #define BUFFER_SIZE 1024
+    #define SERVER_VERSION "Soria/0.0.2b (Unix)"
+#endif
+
+namespace httpStatus{
+    extern std::map<int, std::string> Code;
+}
+
+class Response
+{
+private:
+    /* data */
+    std::string type = httpStatus::Code[200];
+    std::string message;
+    std::map<std::string, std::string> headers;
+    std::map<std::string, std::string> cookies;
+
+    bool isFile = false;
+public:
+
+    Response(const std::string &responseMessage, int responseCode=200, std::map<std::string, std::string> headers = {});
+
+    void addHeader(std::string key, std::string value) {this->headers[key] = value;}
+    void addSessionCookie(std::string key, std::string sessionID) {this->cookies[key] = sessionID;}
+    void setIsFile(std::string contentType, int fileLength);
+    std::string generateResponse();
+};
+
+
+// example of usage
+// Response res = Response("Hello World", 200, {{"Content-Type", "text/html"}});
+// res.addSessionCookie("sessionID", uuid::generateUUIDv4());
+// res.generateResponse()
